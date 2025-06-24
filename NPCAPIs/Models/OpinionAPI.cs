7bae6ea16896f7883a750ea6c0db2daf1eb47789
omarhishamaliassal <102229712+omarhishamaliassal@users.noreply.github.com>
2025-06-24 11:05:42 +0300

    Add files via upload

---
+++ NPCAPIs/Models/OpinionAPI.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NPCAPIs.Models
+{
+    //[Table("Opinion")]
+    public class OpinionAPI
+    {
+        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public decimal OpinionId { get; set; }
+
+        public string Name { get; set; }
+        public string? Email { get; set; }
+        public string MobileNum { get; set; }
+        public string OpinionText { get; set; }
+        
+
+    }
+
+}
