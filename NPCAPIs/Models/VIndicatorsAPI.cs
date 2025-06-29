2627df1d079c533061c28310a98966492f957b22
omarhishamaliassal <102229712+omarhishamaliassal@users.noreply.github.com>
2025-06-29 10:25:43 +0300

    Add files via upload

---
+++ NPCAPIs/Models/VIndicatorsAPI.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NPCAPIs.Models
+{
+    public class VIndicatorsAPI
+    {
+         
+        [Key]
+        public decimal IndId { get; set; }
+        public decimal GovId { get; set; }
+        public string IndName { get; set; }
+        public string GovName { get; set; }
+        public string MonthDesc { get; set; }
+        public string MashoraDesc { get; set; }
+        public DateTime? Date { get; set; }
+
+        public decimal? MonthId { get; set; }
+        public decimal? IndYear { get; set; }
+        public decimal? IndValue { get; set; }
+
+      
+        
+
+    }
+}
